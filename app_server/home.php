1fdfc8e949d9d88b875152a5ffe16de11ef4505c
jose m <josemcarvajal4@gmail.com>
2019-09-12 15:35:46 -0430

    version 0
    creando

---
+++ app_server/home.php
@@ -0,0 +1,54 @@
+ <div class="page-title">
+                        <span class="title">2Fast</span>
+                        <div class="description">Control de Envios de Fast2send.com</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <div class="card-title">
+                                        <div class="title">Bienvenido</div>
+                                    </div>
+                                    <div class="pull-right card-action">
+                                        <div class="btn-group" role="group" aria-label="...">
+                                            <button type="button" class="btn btn-link" data-toggle="modal" data-target="#modalCardProfileExample"><i class="fa fa-code"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <p>
+                                        El 2Fast fue creado para dar seguimiento a sus envíos, facturas, pagos y estados de cuentas<br><br>
+ 
+                                        Elija la opcion que desea realizar:<br><br>
+
+                                    </p>
+                                    <ul class="icons-list">
+
+                                         <li>
+                                            <a href="http://fast2send.com/hosting-cost-calculator/">
+                                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                            <span class="glyphicon-class">Cotizar</span></a>
+                                        </li>
+                                        <li>
+                                            <a href="?page=consultar">
+                                            <span class="glyphicon glyphicon-object-align-left" aria-hidden="true"></span>
+                                            <span class="glyphicon-class">Ver estado de cuenta</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="?page=buscar">
+                                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                                            <span class="glyphicon-class">Tracking</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="?page=historial">
+                                            <span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
+                                            <span class="glyphicon-class">Historial de envios</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
